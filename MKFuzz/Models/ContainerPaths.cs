3e5646acaf0188e14c2fa594a34b79a523d76021
Maksim Kamisarau <no>
2026-04-10 16:01:38 +0300

    added the -g -O0 flags to the fuzz build command to make sure gdb script works properly on the crashes.

--- MKFuzz/Models/ContainerPaths.cs
+++ MKFuzz/Models/ContainerPaths.cs
@@ -9,7 +9,7 @@ public static class ContainerPaths
     public const string OutputMount = "/workspace/hostout";
 
     // Build command templates
-    public const string FuzzBuildPrefix = "cd " + SourceFolder + " &&  CC=afl-clang-fast CXX=afl-clang-fast++ ";
+    public const string FuzzBuildPrefix = "cd " + SourceFolder + " &&  CC=afl-clang-fast CXX=afl-clang-fast++ CFLAGS=\"-g -O0\" CXXFLAGS=\"-g -O0\" ";
     public const string FuzzBuildSuffix = "";
 
     public const string CoverageBuildPrefix = "cd " + SourceFolder + " && CC=clang-18 CXX=clang++-18 CFLAGS=\"-fprofile-instr-generate -fcoverage-mapping -O0 -g\" CXXFLAGS=\"-fprofile-instr-generate -fcoverage-mapping -O0 -g\" LDFLAGS=\"-fprofile-instr-generate -fcoverage-mapping\" ";
